84b44c44367a9b77572045aaae98fab5abd1ffe3
catharinach <catharinach@al.insper.edu.br>
2026-02-21 12:22:14 -0300

    correcoes

--- exe0_cppcheck/main.c
+++ exe0_cppcheck/main.c
@@ -4,8 +4,7 @@
 #define NUM_ELEMENTS 5
 
 int multiplyNumbers(int x, int y) {
-    int product = x * y;
-    return product;
+    return x * y;
 }
 
 int main(void) {
@@ -13,9 +12,12 @@ int main(void) {
     int b = 5; 
     int result = multiplyNumbers(a, b);
 
+    printf("Result of %d * %d = %d\n", a, b, result);
+
     int arr[NUM_ELEMENTS];
     for(int i = 0; i < NUM_ELEMENTS; i++) {
-        arr[i] = i*result;
+        arr[i] = i * result;
+        printf("arr[%d] = %d\n", i, arr[i]);
     }
 
     return 0;
--- exe0_variables/main.c
+++ exe0_variables/main.c
@@ -1,18 +1,22 @@
-#include "stdio.h"
+#include <stdio.h>
 
 void foo(int *a) {
     *a = *a + 1; // acessa ponteiro
 }
 
-void main(void) {
+int main(void) {
     int a = 0;
     int b = 0;
 
-    while (1) {
-        a = foo(&a);
+    while (a <= 10) {
+        foo(&a);
+        printf("a = %d\n", a);
 
         if (a > 5) {
             b = 1;
+            printf("b foi setado para: %d\n", b);
         }
     }
+
+    return 0;
 }
