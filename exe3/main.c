f109320dccdb5e89ae8474a22ee265293ee9843e
catharinach <catharinach@al.insper.edu.br>
2026-02-21 11:51:59 -0300

    exercicios 1 a 5 feitos

--- exe3/main.c
+++ exe3/main.c
@@ -2,9 +2,45 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int LED_PIN = 4;
+const int LED_PIN2 = 6;
+const int BTN_PIN = 28;
+const int BTN_PIN2 = 26;
+  
+
 int main() {
   stdio_init_all();
 
+  gpio_init(LED_PIN);
+  gpio_set_dir(LED_PIN, GPIO_OUT);
+
+  gpio_init(LED_PIN2);
+  gpio_set_dir(LED_PIN2, GPIO_OUT);
+
+  gpio_init(BTN_PIN);
+  gpio_set_dir(BTN_PIN, GPIO_IN);
+  gpio_pull_up(BTN_PIN);
+
+  gpio_init(BTN_PIN2);
+  gpio_set_dir(BTN_PIN2, GPIO_IN);
+  gpio_pull_up(BTN_PIN2);
+
+  bool btn_state = true;
+  bool btn_state2 = true;
+
   while (true) {
+
+    bool current_btn_state = gpio_get(BTN_PIN);
+    bool current_btn_state2 = gpio_get(BTN_PIN2);
+
+    if (!current_btn_state && btn_state) {
+      gpio_put(LED_PIN, !gpio_get(LED_PIN));
+    } 
+    if (!current_btn_state2 && btn_state2) {
+      gpio_put(LED_PIN2, !gpio_get(LED_PIN2));
+    }
+    
+    btn_state = current_btn_state;
+    btn_state2 = current_btn_state2;
   }
 }
--- exe4/main.c
+++ exe4/main.c
@@ -2,10 +2,47 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int LED_PIN = 5;
+const int LED_PIN2 = 8;
+const int LED_PIN3 = 11;
+const int LED_PIN4 = 15;
+const int BTN_PIN = 28;
+
 int main() {
   stdio_init_all();
 
+  gpio_init(LED_PIN);
+  gpio_set_dir(LED_PIN, GPIO_OUT);
+
+  gpio_init(LED_PIN2);
+  gpio_set_dir(LED_PIN2, GPIO_OUT);
+
+  gpio_init(LED_PIN3);
+  gpio_set_dir(LED_PIN3, GPIO_OUT);
+
+  gpio_init(LED_PIN4);
+  gpio_set_dir(LED_PIN4, GPIO_OUT);
+
+  gpio_init(BTN_PIN);
+  gpio_set_dir(BTN_PIN, GPIO_IN);
+  gpio_pull_up(BTN_PIN);
+
   while (true) {
-    // Use delay de 300 ms entre os estados!
+    if (!gpio_get(BTN_PIN)) {
+      gpio_put(LED_PIN, 1);
+      sleep_ms(300);
+      gpio_put(LED_PIN, 0);
+      gpio_put(LED_PIN2, 1);
+      sleep_ms(300);
+      gpio_put(LED_PIN2, 0);
+      gpio_put(LED_PIN3, 1);
+      sleep_ms(300);
+      gpio_put(LED_PIN3, 0);
+      gpio_put(LED_PIN4, 1);
+      sleep_ms(300);
+      gpio_put(LED_PIN4, 0);      
+      while (!gpio_get(BTN_PIN)) {
+      };
+    }
   }
 }
