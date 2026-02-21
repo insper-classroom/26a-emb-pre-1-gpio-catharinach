051a556b8403a2fc864f5f8b0809dc7f798d0259
catharinach <catharinach@al.insper.edu.br>
2026-02-21 12:10:40 -0300

    .

--- exe0_cppcheck/main.c
+++ exe0_cppcheck/main.c
@@ -5,15 +5,17 @@
 
 int multiplyNumbers(int x, int y) {
     int product = x * y;
+    return product;
 }
 
 int main(void) {
-    int a, b = 5; //uninitvar, legacyUninitvar
+    int a = 5; 
+    int b = 5; 
     int result = multiplyNumbers(a, b);
 
     int arr[NUM_ELEMENTS];
-    for(int i = 0; i < NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
-        arr[i] = i;
+    for(int i = 0; i < NUM_ELEMENTS; i++) {
+        arr[i] = i*result;
     }
 
     return 0;
